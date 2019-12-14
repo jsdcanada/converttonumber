b1d97eda3c582e19116f1adb8bff7a37c2924a61
jsdcanada <jagdishchandra@gmail.com>
2019-12-14 16:31:55 +0000

    applied code review comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     }
     public static void main(String[] args) {
 	// write your code here
-        int a = convertToNumber("10");
+        int a = convertToNumber("10"); // done better
         int b = convertToNumber("20");
         System.out.println(a * b);
     }
